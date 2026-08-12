3e52520df378e91049425573abbd8da01ca823d4
danialcala94 <danielalcalavalera@gmail.com>
2026-08-13 06:29:00 +0800

    Added 'from-datetime' class method to 'PublicationDate' dataclass to simplify the way we handle this. Poetry lock file updated. New version.

--- src/bufferpy/inputs/dataclasses.py
+++ src/bufferpy/inputs/dataclasses.py
@@ -2,6 +2,10 @@ from dataclasses import dataclass
 from datetime import datetime, timezone
 
 
+"""
+TODO: This class should be avoided and use
+datetime utc instead directly...
+"""
 @dataclass
 class PublicationDate:
     """
@@ -14,6 +18,27 @@ class PublicationDate:
     day: int
     hour: int
     minute: int
+
+
+    @classmethod
+    def from_datetime(
+        cls,
+        value: datetime,
+    ) -> 'PublicationDate':
+        """
+        Get a `PublicationDate` class instance with
+        the datetime provided as `value`, that will
+        be considered UTC.
+        """
+        value = value.astimezone(timezone.utc)
+
+        return cls(
+            year = value.year,
+            month = value.month,
+            day = value.day,
+            hour = value.hour,
+            minute = value.minute,
+        )
 
     @property
     def as_iso8601(
